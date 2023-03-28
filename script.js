514e2bb25ca2db3ce3b446cb4c669f8b34c9bd3c
vladh <hadyak305246@gmail.com>
2023-03-28 00:18:57 -0600

    Add 'addition' and 'subtraction' that operates with multiple numbers

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Calculator by Vlad Hadyak
 
 const result = document.querySelector(".result");
+const expression = document.querySelector(".expression");
+
+const divide = document.querySelector("#division");
+const multiply = document.querySelector("#multiplication");
+const subtract = document.querySelector("#subtraction");
+const add = document.querySelector("#addition");
+const equal = document.querySelector("#equals");
+
 const numButtons = [];
+
+let numericValue = 0;
+
+let operator = "";
+
+let addClicked = false;
+let subtractClicked = false;
+let equalClicked = false;
 
 // Include 10 number buttons
 for (let i = 0; i <= 9; i++) {
@@ -12,50 +28,85 @@ function displayNumbers() {
   numButtons.forEach((button) => {
     const number = button.textContent;
     button.addEventListener("click", () => {
-      let finalResult = result.textContent += number;
+
+      if (addClicked) {                // If add button was clicked before entering second number
+        result.textContent = "";
+        addClicked = false;
+      } else if (subtractClicked) {
+        result.textContent = "";
+        subtractClicked = false;
+      };
+
+      let finalNumber = result.textContent += number;
       // If numbers exceed over 15 digits in length on display, then restrict it from displaying more numbers
-      if (finalResult.toString().length > 15) {                      
-         finalResult = finalResult.substring(0, 15);
-         console.log("Sorry! You reached maximum number of digits");
-      }
-      result.textContent = finalResult;
+      if (finalNumber.toString().length > 15) {                      
+         finalNumber = finalNumber.substring(0, 15);
+      };
+      result.textContent = finalNumber; // finalNumber means if you are done with entering anymore digits and ready to use operators
+      numericValue = parseFloat(finalNumber); // Converts 'string' value to 'numeric' value
     });   
   });
 };
 displayNumbers();
 
+let runningTotal = 0;
 
-let num1 = 6;
-let num2 = 3;
-let operator = "+"
-
-function addition(num1, num2) {
-  return num1 + num2;
+function addExpression(operator) {
+  if (operator == "+") {
+    expression.textContent += `${numericValue} ${operator}`;
+  } else if (operator == "-") {
+    expression.textContent += `${numericValue} ${operator}`;
+  };
+  return operator;
 };
 
-function subtraction(num1, num2) {
-  return num1 - num2;
-};
+function addition() {
+  add.addEventListener("click", () => {
+    operator = "+";
+    expression.textContent += `${numericValue} ${addExpression("+")} `;
+    addClicked = true;
 
-function multiplication(num1, num2) {
-  return num1 * num2;
-};
+    // Updated result every time first 2 numbers are added
+    runningTotal += numericValue;       
+    result.textContent = runningTotal;
 
-function division(num1, num2) {
-  return num1 / num2;
+  });
+  return runningTotal += numericValue;
 };
+addition();
 
-function operate(operator, num1, num2) {
- if (operator === "+") {
-  return addition(num1, num2);
- } else if (operator === "-") {
-  return subtraction(num1, num2);
- } else if (operator === "*") {
-  return multiplication(num1, num2);
- } else if (operator === "/") {
-  return division(num1, num2);
- };
+function subtraction() {
+  subtract.addEventListener("click", () => {
+    //console.log(runningTotal);
+    operator = "-";
+    expression.textContent += `${numericValue} ${addExpression("-")} `;
+    subtractClicked = true;
+   
+    if (runningTotal === 0) {
+      runningTotal = numericValue;
+      result.textContent = runningTotal;
+    } else {
+      runningTotal -= numericValue;
+      result.textContent = runningTotal;
+    };
+  });
+  return runningTotal -= numericValue;
 };
+subtraction();
 
-//result.textContent = operate(operator, num1, num2);  //Display result in the input text box
+
+function operate() {
+  equal.addEventListener("click", () => {
+    expression.textContent += ` ${numericValue}`;
+    if (operator == "+") {
+      result.textContent = addition();
+      addClicked = false;
+    } else if (operator == "-") {
+      result.textContent = subtraction();
+      subtractClicked = false;
+    };
+  });
+};
+operate();
+
 
